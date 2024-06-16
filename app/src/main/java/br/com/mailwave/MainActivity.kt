7157a0710fbb02d7005a973d6c749458e4bf3137
Henry Thuler <henrythuler2003@outlook.com>
2024-06-16 19:21:34 -0300

    feat(components): options

--- app/src/main/java/br/com/mailwave/MainActivity.kt
+++ app/src/main/java/br/com/mailwave/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.mailwave.screens.HomeScreen
+import br.com.mailwave.screens.MessageScreen
+import br.com.mailwave.screens.SplashScreen
 import br.com.mailwave.ui.theme.MailWaveTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +25,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "splash"
+                    ) {
+                        composable(route = "splash") { SplashScreen(navController = navController) }
+                        composable(route = "home") { HomeScreen(navController = navController) }
+                        composable(route = "message/create") { MessageScreen(navController = navController) }
+                        composable(route = "message/read") { MessageScreen(navController = navController) }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MailWaveTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/br/com/mailwave/ui/theme/ChooseOptions.kt
@@ -0,0 +1,106 @@
+package br.com.mailwave.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChooseOptions(choseOption: String){
+
+    var allButtonColor by remember {
+        mutableStateOf(Color(0x00000000))
+    }
+
+    val inboxButtonColor by remember {
+        mutableStateOf(Color(0x00000000))
+    }
+
+    val sentButtonColor by remember {
+        mutableStateOf(Color(0x00000000))
+    }
+
+    val unreadButtonColor by remember {
+        mutableStateOf(Color(0x00000000))
+    }
+
+    Box(modifier = Modifier.fillMaxWidth()){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = Color(0xFF121212))
+                .height(50.dp)
+            ,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Button(onClick = { allButtonColor. }, colors = ButtonDefaults.buttonColors(contentColor = allButtonColor)) {
+                Text(
+                    text = "All",
+                    fontSize = 16.sp,
+                    fontFamily = Inter,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White
+                )
+            }
+
+            VerticalDivider(modifier = Modifier.height(25.dp), thickness = 1.dp, color = Color(0XFF292929))
+
+            Text(
+                text = "Inbox",
+                fontSize = 16.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+
+            VerticalDivider(modifier = Modifier.height(25.dp), thickness = 1.dp, color = Color(0XFF292929))
+
+            Text(
+                text = "Sent",
+                fontSize = 16.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+
+            VerticalDivider(modifier = Modifier.height(25.dp), thickness = 1.dp, color = Color(0XFF292929))
+
+            Text(
+                text = "Unread",
+                fontSize = 16.sp,
+                fontFamily = Inter,
+                fontWeight = FontWeight.Medium,
+                color = Color.White
+            )
+
+        }
+
+    }
+
+}
